23c44459b252a1453ce982e09edbd27d7cf97c6d
root <KingJamin@hotmail.com>
2018-10-10 18:38:54 -0500

    Added server-side script and some more touches to client side

---
+++ jgpychatserver.py
@@ -0,0 +1,54 @@
+# Python program to implement server side of chat room.
+import socket 
+import select 
+import sys 
+from thread import *
+#Cmd line server for jgpychat
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) 
+#requires 3 arguments on cmd line
+if len(sys.argv) != 3: 
+    print ("Correct usage: script, IP address, port number")
+    exit() 
+#setup server variables
+ip = str(sys.argv[1]) 
+port = int(sys.argv[2]) 
+server.bind((ip, port))
+#listen for clients 
+server.listen(100) 
+clients = [] 
+#Client connection function
+def clientThread(conn, addr): 
+    conn.send("Welcome to the chat!") 
+    while True: 
+            try: 
+                message = conn.recv(2048) 
+                if message: 
+                    print ("<" + addr[0] + "> " + message )
+                    sendMessage = "<" + addr[0] + "> " + message 
+                    broadcast(sendMessage, conn) 
+                else: 
+                    remove(conn) 
+            except: 
+                continue
+#Broadcast messages
+def broadcast(message, connection): 
+    for clients in clients: 
+        if clients!=connection: 
+            try: 
+                clients.send(message) 
+            except: 
+                clients.close() 
+                remove(clients) 
+#Remove connection
+def remove(connection): 
+    if connection in clients: 
+        clients.remove(connection)
+#Start server loop 
+while True: 
+    conn, addr = server.accept() 
+    clients.append(conn)   
+    print (addr[0] + " connected")
+    start_new_thread(clientThread,(conn,addr))     
+conn.close() 
+server.close() 
